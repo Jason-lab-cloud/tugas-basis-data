b50fab22c7fed91ec4b824b97c66efed7d9e5864
C14240018 <C14240018@john.petra.ac.id>
2025-06-16 16:29:49 +0700

    Jason test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.example.tugasbasisdata {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.zaxxer.hikari;
+    requires java.sql;
+    requires org.slf4j;
 
 
     opens com.example.tugasbasisdata to javafx.fxml;

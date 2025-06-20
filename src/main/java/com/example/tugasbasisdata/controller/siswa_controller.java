bc156521964f81d57dcb06310506c62f23343ebe
C14240018 <C14240018@john.petra.ac.id>
2025-06-20 14:50:57 +0700

    J

--- src/main/java/com/example/tugasbasisdata/controller/siswa_controller.java
+++ src/main/java/com/example/tugasbasisdata/controller/siswa_controller.java
@@ -1,4 +1,10 @@
 package com.example.tugasbasisdata.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class siswa_controller {
+    @FXML
+    private Button Biodata;
+    
 }
